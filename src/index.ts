81daebfe839655ad78ca4196bf374accf3b5ecf2
JongWoo Park <jjongwoo.dev@gmail.com>
2020-07-17 18:38:39 +0900

    fix: DEV, prod 구분 못하는 오류 수정

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import server from "./server";
 
 // 포트 설정하기. TODO: http 통신시 80으로 바꾸기. https는 443
 const { PORT } = process.env;
-const port = 3000 || PORT;
+const port = PORT || 3000;
 
 // Server 열기
 server.listen(port, () => {
